4c2784134a7be082f00ff16ea597a393c5b04dff
Aisla <severoaisla@gmail.com>
2025-06-27 15:33:42 -0300

    Create diccarlos.py

---
+++ diccarlos.py
@@ -0,0 +1,11 @@
+aluno = {
+    "nome" : "Carlos Eduardo",
+    "idade" : "16",
+    "turma" : "2A"
+
+}
+
+aluno["disciplina_favorita"] = "Matematica" 
+aluno["idade"] = "17" 
+print(aluno)
+print(f"turma: {aluno['turma']}")
