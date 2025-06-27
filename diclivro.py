f2adf77316cee9c501ff07f641ea06c412444da1
Aisla <severoaisla@gmail.com>
2025-06-27 15:20:42 -0300

    Create diclivro.py

---
+++ diclivro.py
@@ -0,0 +1,11 @@
+livro = {
+    "titulo" : "O Pequeno Príncipe", 
+    "autor" : "Antoine de Saint-Exupéry",
+    "ano_publicacao" : "1943",
+     "genero" : "fabula"
+
+}
+
+print(livro)
+aluno["genero"] = "fabula" 
+print(livro)
