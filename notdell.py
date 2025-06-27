ed1c740ead2ba28ba8d6c318d770552e9959626d
Aisla <severoaisla@gmail.com>
2025-06-27 14:37:41 -0300

    Create dicnotdell.py

---
+++ notdell.py
@@ -0,0 +1,10 @@
+produto = {
+  "nome" : "Notebook Dell",
+  "preco" : "4500.00",
+  "estoque" : "15",
+  "cor" : "prata"
+}
+
+print(produto)
+print(f"nome: {produto['nome']}")
+print(f"preço: {produto['preco']}")
